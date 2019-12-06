ec85181a0b6dd323532f38a4d818b2fdd4433307
Paweł Szot <pszot@pgs-soft.com>
2019-12-06 15:20:59 +0100

    improvements

--- src/InsertActionProvider.ts
+++ src/InsertActionProvider.ts
@@ -10,14 +10,37 @@ interface CommandInput {
     range: Range;
 }
 
+interface Entry {
+    key: string;
+    value: string;
+}
+
 export class InsertActionProvider implements CodeActionProvider {
     readonly actionName = "r_flutter.insert";
     readonly filter: DocumentFilter = { language: "dart", scheme: "file" };
-    readonly className = 'StringsBinding';
+    readonly className = 'I18n';
     readonly regex = RegExp(`^The getter \'(.*)\' isn\'t defined for the class \'${this.className}\'.`);
 
-    extractName(context: CodeActionContext): [string, Diagnostic] | null {
-        for (const obj of context.diagnostics) {
+    async runFileCheck() {
+        let diagnosticsPerFile = vs.languages.getDiagnostics();
+
+        let entries = Array<Entry>();
+
+        for (const file of diagnosticsPerFile) {
+            let array = file[1];
+            for (const diag of array) {
+                let result = this.extractName([diag]);
+                if (result) {
+                    entries.push({ key: result[0], value: `[TRANSLATE] ${result[0]}` });
+                }
+            }
+        }
+
+        await this.addEntriesToDefaultLocale(entries);
+    }
+
+    extractName(diags: Diagnostic[]): [string, Diagnostic] | null {
+        for (const obj of diags) {
             if (obj.code === "undefined_getter") {
                 const result = this.regex.exec(obj.message);
                 if (result && result.length === 2) {
@@ -29,7 +52,7 @@ export class InsertActionProvider implements CodeActionProvider {
         return null;
     }
     provideCodeActions(document: TextDocument, range: Range, context: CodeActionContext): CodeAction[] {
-        const result = this.extractName(context);
+        const result = this.extractName(context.diagnostics);
         if (result) {
             const action = new CodeAction("I18n: add localization string", CodeActionKind.QuickFix);
             const input: CommandInput = {
@@ -63,20 +86,24 @@ export class InsertActionProvider implements CodeActionProvider {
     async insertAsyncThrowing(input: CommandInput): Promise<void> {
         const key = input.name;
 
-        const value = await window.showInputBox({
-            prompt: "Please enter key for the new localization value",
+        var value = await window.showInputBox({
+            prompt: "Please enter value for new key",
             placeHolder: "Value"
         });
 
-        if (!key || !value) {
+        if (!key) {
             window.showInformationMessage(`Adding key was cancelled.`);
             return;
         }
 
-        await this.addEntryToDefaultLocale(key, value);
+        if (!value) {
+            value = `[TRANSLATE] ${key}`;
+        }
+
+        await this.addEntriesToDefaultLocale([{ key, value }]);
     }
 
-    async addEntryToDefaultLocale(key: string, value: string): Promise<void> {
+    async addEntriesToDefaultLocale(entries: Array<Entry>): Promise<void> {
         let root = vs.workspace.workspaceFolders![0].uri;
         let pubspecPath = path.join(root.fsPath, 'pubspec.yaml');
 
@@ -84,11 +111,18 @@ export class InsertActionProvider implements CodeActionProvider {
         let intlPath = path.join(root.fsPath, config.r_flutter.intl);
 
         let strings: { [id: string]: any } = JSON.parse(fs.readFileSync(intlPath, 'utf8'));
-        strings[key] = value;
+
+        for (let entry of entries) {
+            strings[entry.key] = entry.value;
+        }
+
+
         fs.writeFileSync(intlPath, JSON.stringify(strings, null, '  '));
         //vs.commands.executeCommand("workbench.action.reloadWindow");
 
-        cp.exec('flutter generate', { cwd: root.fsPath }, (err, stdout, stderr) => {
+        let command = vs.workspace.getConfiguration('r_flutter').get<String>("shellCommand")!;
+
+        cp.exec(command.toString(), { cwd: root.fsPath }, (err, stdout, stderr) => {
             if (err != null) {
                 window.showErrorMessage('Failed to add item.');
             } else {
--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vs from "vscode";
-
+import * as cp from 'child_process';
 
 import { InsertActionProvider } from "./InsertActionProvider";
 
@@ -12,6 +12,22 @@ export function activate(context: vs.ExtensionContext) {
     subscriptions.push(vs.commands.registerCommand(provider.actionName, (input) => {
         provider.insertAsync(input);
     }));
+
+    subscriptions.push(vs.commands.registerCommand("r_flutter.runRegenerate", runRegenerate));
+    subscriptions.push(vs.commands.registerCommand("r_flutter.runFileCheck", () => provider.runFileCheck()));
+}
+
+function runRegenerate() {
+    let command = vs.workspace.getConfiguration('r_flutter').get<String>("shellCommand")!;
+
+    let root = vs.workspace.workspaceFolders![0].uri;
+    cp.exec(command.toString(), { cwd: root.fsPath }, (err, stdout, stderr) => {
+        if (err != null) {
+            vs.window.showErrorMessage(`Failed to regenerate: ${err}`);
+        } else {
+            vs.window.showInformationMessage('File regenerated.');
+        }
+    });
 }
 
 export function deactivate() { }
